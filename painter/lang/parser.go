45b8b389e0b586c7b75a46db64dbd21f9474f313
hrystynaa <yaskulskahrystyna@gmail.com>
2023-05-02 09:31:10 +0300

    Refactor parser code

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -14,16 +14,22 @@ import (
 type Parser struct {
 	bgRect  *painter.BgRectangle
 	backOp  painter.Operation
-	move    painter.Operation
+	move    []painter.Operation
 	figures []*painter.Figure
 	res     []painter.Operation
+	update  painter.Operation
+	updated bool
 }
 
 func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
+	p.update = nil
+	p.res = nil
+	if p.backOp == nil {
+		p.backOp = painter.OperationFunc(painter.WhiteFill)
+	}
+
 	scanner := bufio.NewScanner(in)
 	scanner.Split(bufio.ScanLines)
-
-	p.bgRect, p.backOp, p.figures, p.res = nil, nil, nil, nil
 
 	for scanner.Scan() {
 		commandLine := scanner.Text()
@@ -34,27 +40,40 @@ func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
 		if err != nil {
 			return nil, err
 		}
-
-		if len(p.res) == 1 {
-			if p.backOp != nil {
-				p.res = append(p.res, p.backOp)
-			}
-
-			if p.bgRect != nil {
-				p.res = append(p.res, p.bgRect)
-			}
-
-			if p.move != nil {
-				p.res = append(p.res, p.move)
-			}
-
-			for _, figure := range p.figures {
-				p.res = append(p.res, figure)
-			}
-			p.res = append(p.res[1:], p.res[0])
+		if p.updated {
+			p.updated = false
+			p.res = p.genOperations()
 		}
 	}
+
 	return p.res, scanner.Err()
+}
+
+func (p *Parser) genOperations() []painter.Operation {
+	var res []painter.Operation
+
+	if p.backOp != nil {
+		res = append(res, p.backOp)
+	}
+
+	if p.bgRect != nil {
+		res = append(res, p.bgRect)
+	}
+
+	if p.move != nil {
+		res = append(res, p.move...)
+		p.move = nil
+	}
+
+	for _, figure := range p.figures {
+		res = append(res, figure)
+	}
+
+	if p.update != nil {
+		res = append(res, p.update)
+	}
+
+	return res
 }
 
 func (p *Parser) parse(commandLine string) error {
@@ -67,7 +86,9 @@ func (p *Parser) parse(commandLine string) error {
 		if err != nil {
 			return err
 		}
-		arg = arg * 800.0
+		if arg > 0 && arg < 1 {
+			arg = arg * 800.0
+		}
 		args = append(args, int(arg))
 	}
 	switch operation {
@@ -76,18 +97,20 @@ func (p *Parser) parse(commandLine string) error {
 	case "green":
 		p.backOp = painter.OperationFunc(painter.GreenFill)
 	case "update":
-		p.res = p.res[:0]
-		p.res = append(p.res, painter.UpdateOp)
+		p.updated = !p.updated
+		p.update = painter.UpdateOp
 	case "bgrect":
 		p.bgRect = &painter.BgRectangle{X1: args[0], Y1: args[1], X2: args[2], Y2: args[3]}
 	case "figure":
 		figure := &painter.Figure{X: args[0], Y: args[1]}
 		p.figures = append(p.figures, figure)
 	case "move":
-		p.move = &painter.Move{X: args[0], Y: args[1], Figures: p.figures}
+		moveOp := &painter.Move{X: args[0], Y: args[1], Figures: p.figures}
+		p.move = append(p.move, moveOp)
 	case "reset":
-		p.figures = p.figures[:0]
+		p.figures = nil
 		p.bgRect = nil
+		p.move = nil
 		p.backOp = painter.OperationFunc(painter.ResetScreen)
 	default:
 		return errors.New("Failed")
